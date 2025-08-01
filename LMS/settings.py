ecf42a6f1b336c1fe0f599218ded24935920a2da
Kanishka Rani <kanishka22043@gmail.com>
2025-08-01 22:17:23 +0530

    Add ERD

--- LMS/settings.py
+++ LMS/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'accounts.apps.AccountsConfig',
     'inventory.apps.InventoryConfig',
-    'borrowing.apps.BorrowingConfig'
+    'borrowing.apps.BorrowingConfig',
+    'django_extensions',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
@@ -120,13 +122,19 @@ LOGIN_URL = '/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 STATIC_URL = "static/"
 
-STATICFILES_DIRS=[
-    os.path.join('static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+GRAPH_MODELS ={
+'all_applications': True,
+'graph_models': True,
+}
